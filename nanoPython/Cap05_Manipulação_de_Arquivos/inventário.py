2731d5afe9890bb59ea671d748da2377a34484ff
Gustavo Nogueira <gustavoin94@gmail.com>
2022-07-26 15:30:56 -0300

    Atualizações 27/07

--- nanoPython/Cap05_Manipulação_de_Arquivos/inventário.py
+++ nanoPython/Cap05_Manipulação_de_Arquivos/inventário.py
@@ -1,17 +1,16 @@
-inventario={}
-opcao=int(input('Digite: \n "1" para registrar ativo\n "2" para persistir em arquivo\n "3" para exibir ativos armazenados: '))
+from funcoesArquivos import *
+inventario = {}
+opcao = chamarMenu()
 while opcao>0 and opcao<4:
-    if opcao==1:
-        resp='S'
-        while resp=='S':
-            inventario[input('Digite o número patrimonial: ')]=[input('Digite a data da última atualização: '), input('Digite a descrição: '), input('Digite o departamento: ')]
-            resp=input('Digite "S" para continuar: ').upper()
-    elif opcao==2:
-        with open('inventario.csv','a')as inv:
-            for chave,valor in inventario.items():
-                inv.write(chave+';'+valor[0]+';'+valor[1]+';'+valor[2]+ '\n')
-                print('Persistido com sucesso!')
-    elif opcao==3:
-        with open('inventario.csv','r')as inv:
-            print(inv.readline())
-    opcao = int(input('Digite: \n "1" para registrar ativo\n "2" para persistir em arquivo\n "3" para exibir ativos armazenados: '))
+    if opcao == 1:
+        registrar(inventario)
+    elif opcao == 2:
+        persistir(inventario)
+    elif opcao == 3:
+        resultado=exibir()
+        for linha in resultado:
+            lista=linha.split(';')
+            print('Data.........: ',lista[1])
+            print('Descrição:...: ',lista[2])
+            print('Departamento.: ',lista[3])
+    opcao = chamarMenu()
