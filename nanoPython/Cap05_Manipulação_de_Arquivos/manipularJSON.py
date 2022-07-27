c6526080dd1c5da7de0cfad1547b8111bd46ac56
Gustavo Nogueira <gustavoin94@gmail.com>
2022-07-26 21:09:22 -0300

    Fim 26/07

--- nanoPython/Cap05_Manipulação_de_Arquivos/manipularJSON.py
+++ nanoPython/Cap05_Manipulação_de_Arquivos/manipularJSON.py
@@ -1,20 +1,10 @@
-import json
-inventario={}
-opcao=int(input('Digite: \n "1" para registrar ativo\n "2" para exibir ativos armazenados: '))
+from funcoesJSON import *
+
+inventario=lerArquivo('inventarioJSON.json')
+opcao=chamarMenu()
 while opcao>0 and opcao<3:
     if opcao==1:
-        resp='S'
-        while resp=='S':
-            inventario[input('Digite o número patrimonial: ')]=[input('Digite a data da última atualização: '),input('Digite a descrição: '),input('Digite o departamento: ')]
-            resp=input('Digite "S" para continuar: ').upper()
-        with open('inventarioJSON.json','w')as arq_json:
-            json.dump(inventario,arq_json)
-        print('JSON gerado!')
+        print(registrar(inventario,'inventarioJSON.json'))
     elif opcao==2:
-        with open('inventarioJSON.json','r')as arq_json:
-            resultado=json.load(arq_json)
-            for chave,dado in resultado.items():
-                print(f'Data.........: {dado[0]}')
-                print(f'Descrição....: {dado[1]}')
-                print(f'Departamento.: {dado[2]}')
-    opcao=int(input('Digite: \n "1" para registrar ativo\n "2" para exibir ativos armazenados: '))
+        exibir('inventarioJSON.json')
+    opcao=chamarMenu
